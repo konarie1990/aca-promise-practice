c53fdcda70fffe7a12e43ab0e2db154b445463fe
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-03-29 17:34:21 -0500

    displaying JSON results

--- src/Users.js
+++ src/Users.js
@@ -1,19 +1,66 @@
-import React, { Component, Fragment } from 'react'
-import { Button } from '@material-ui/core'
+import React, { Component, Fragment } from "react";
+import { Button } from "@material-ui/core";
+import { connect } from "react-redux";
+
+const styles = {
+  backgroundColor: "pink",
+  listStyle: "none",
+  margin: "5px",
+  color: "gray"
+};
 
 class Users extends Component {
+  getUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(results => results.json())
+      .then(data => {
+        console.log(data);
+        this.props.setUsers(data);
+      });
+  };
   render() {
     return (
       <Fragment>
         <div>
           <h3>Press the button to get users and display them below</h3>
-          <Button onClick={this.getUsers} variant="contained" color="primary">Get Users</Button>
+          <Button onClick={this.getUsers} variant="contained" color="primary">
+            Get Users
+          </Button>
         </div>
         <div className="users-block">
+          {this.props.users.map(user => (
+            <form key={user.id}>
+              <ul className="square-box" style={styles}>
+                <li>name: {user.name}</li>
+                <li>username: {user.username}</li>
+                <li>email: {user.email}</li>
+                <li>website: {user.website}</li>
+              </ul>
+            </form>
+          ))}
         </div>
       </Fragment>
-    )
+    );
   }
 }
 
-export default Users
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setUsers: displayUsers =>
+      dispatch({
+        type: "SET_USERS",
+        value: displayUsers
+      })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Users);
